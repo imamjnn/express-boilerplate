6fff3c4348c611dca585b9076a6e4ac40ade7ad7
Imam Jinani <immjnn@gmail.com>
2024-02-24 14:38:34 +0700

    add rate limiter

--- src/app.js
+++ src/app.js
@@ -3,15 +3,38 @@ import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import cors from 'cors';
 import router from './router';
+import {rateLimit} from 'express-rate-limit';
 
 const app = express();
 const port = 3000;
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  handler: (req, res) => {
+    res.json({status: 0, message: 'Too many requests, please try again later.'});
+  },
+});
+
+app.use(limiter);
 app.disable('x-powered-by');
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(router);
+
+// custom 404
+app.use((req, res, next) => {
+  res.status(404).send("Sorry can't find that!");
+});
+
+// custom error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,6 +1,9 @@
+import dayjs from 'dayjs';
+
 export const home = async (req, res) => {
   console.log('home');
   res.json({
     message: 'Simple Express boilerplate by ImmJnn',
+    date: dayjs().format('DD MMM YYYY, HH:mm:ss'),
   });
 };
